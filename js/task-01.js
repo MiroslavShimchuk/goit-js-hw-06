4414412478aba30ba048ddd00cb6b4adba2f852d
Miroslav34 <miroslavshimchuk@gmail.com>
2022-02-21 23:54:22 +0200

    задача1

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const NumberOfCategories = document.querySelectorAll('.item');
+console.log('Number of categories:', NumberOfCategories.length);
+console.log(NumberOfCategories[0].textContent);
+
+
+
+// const Categories = NumberOfCategories.forEach(category => {
+//     for (let i=0; i <= NumberOfCategories.length; i++) {
+//         console.log('Category:',category.querySelector('.h2'));
+//     }
+// })
