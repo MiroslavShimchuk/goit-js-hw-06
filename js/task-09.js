2612a21d707ea20befb24b6e42560fbaabb3aa78
Miroslav34 <miroslavshimchuk@gmail.com>
2022-02-23 18:23:04 +0200

    9-task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,21 @@
+const changeColorBtnNode = document.querySelector('.change-color');
+const bodyNode = document.querySelector('body');
+const spanNode = document.querySelector('.color');
+
+changeColorBtnNode.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  bodyNode.style.background = color;
+  spanNode.textContent = color;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
 }
+
+
+// function getRandomHexColor() {
+//   bodyNode.setAttribute('style', `background: #${Math.floor(Math.random() * 16777215).toString(16)}`);
+//   console.log(bodyNode.getAttribute('style'));
+//   spanNode.textContent = bodyNode.getAttribute('style').slice(11);
+// }
