8081db1664398dda07cb9d2a6e2fd218206733ad
Miroslav34 <miroslavshimchuk@gmail.com>
2022-02-24 00:46:45 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -3,6 +3,8 @@ const outNode = document.querySelector("#text");
 
 inputNode.addEventListener("input", inputStatus);
 
+outNode.setAttribute("style", `font-size: ${inputNode.value}px`);
+
 function inputStatus(e) {
   outNode.setAttribute("style", `font-size: ${inputNode.value}px`);
 }
